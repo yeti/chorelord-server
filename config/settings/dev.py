f16fa362954d3ec75b5eee95214f8868ac5b521d
anyatran <anya@yeti.co>
2016-02-24 16:32:44 -0800

    init

---
+++ config/settings/dev.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "&[Ldevelopment.secret_key>'rwOj|NaZACYsV"
+
+ALLOWED_HOSTS = ['']
